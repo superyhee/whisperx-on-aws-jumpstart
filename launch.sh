04333cbd37652153f9be11116972ea757e89326e
YanJun <jnyanm@amazon.com>
2024-04-03 07:38:43 +0000

    update launch.sh

--- launch.sh
+++ launch.sh
@@ -4,7 +4,38 @@
 cd /home/ubuntu/whisper/ || exit
 
 # Install requirements
-pip3 install -r requirements.txt
+#pip3 install -r requirements.txt
+
+add_notification_for_s3(){
+   local bucket_name=$(read_env_var "BUCKET_NAME")
+   local function_name=$(read_env_var "LAMBDA_NAME")
+   local region=$(read_env_var "SQS_QUEUE_URL" | awk -F'.' '{print $2}')
+
+   # 写一段脚本调用awscli命令，为bucket_name增加一个事件通知，当文件创建的时候触发 function_name
+   # 获取 Lambda 函数的 ARN
+    function_arn=$(aws lambda get-function --function-name "$function_name" --query 'Configuration.FunctionArn' --region $region --output text)
+
+    # 创建事件通知配置
+    notification_config=$(cat <<EOF
+{
+  "LambdaFunctionConfigurations": [
+    {
+      "LambdaFunctionArn": "$function_arn",
+      "Events": [
+        "s3:ObjectCreated:*"
+      ]
+    }
+  ]
+}
+EOF
+)
+    # 添加事件通知
+    aws s3api put-bucket-notification-configuration \
+        --bucket "$bucket_name" \
+        --notification-configuration "$notification_config"
+        
+    echo "事件通知已添加到存储桶 $bucket_name，当创建新对象时将触发 Lambda 函数 $function_name"
+}
 
 # Function to read a variable from the .env file
 read_env_var() {
@@ -41,6 +72,7 @@ start_event_workflow() {
 }
 
 # Start the required services
+add_notification_for_s3
 start_api_server
 start_demo
 start_event_workflow
